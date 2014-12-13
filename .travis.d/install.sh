e8c4d306f179d3e45865676340aae61f323e2aaf
Christopher Redekop <chris.redekop@gmail.com>
2014-12-13 10:40:26 -0500

    [ARCH-152] Build-time db system property not propagated across transitive dependencies; another attemp to filter build logs.

--- .travis.d/install.sh
+++ .travis.d/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -ev
 
-mvn -P ossrh-down versions:update-properties -DincludeProperties=jdbc-jboss-modules.version,jxpath-jboss-module.version,dcm4che.version,dcm4chee-storage.version | grep -v '^Downloaded' | grep -v '^Downloading' | grep -v '^Props'
+mvn -P ossrh-down versions:update-properties -DincludeProperties=jdbc-jboss-modules.version,jxpath-jboss-module.version,dcm4che.version,dcm4chee-storage.version | grep -v 'Downloaded: ' | grep -v 'Downloading: ' | grep -v 'Props: ' | grep -v '[[:digit:]]\+ KB'
 mvn -P ossrh-down,db-all install -DskipTests=true
