857d203461c0a0897518cd5a6e64e61f03ddb08c
gunterze <gunterze@gmail.com>
2014-12-15 09:46:52 +0100

    Revert "[ARCH-152] Build-time db system property not propagated across transitive dependencies; filter build logs."
    This reverts commit d50f0079e2dcde713abc8df3eaaf5f7c5aef1e25.

--- .travis.d/install.sh
+++ .travis.d/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 
-mvn -P ossrh-down versions:update-properties -DincludeProperties=jdbc-jboss-modules.version,jxpath-jboss-module.version,dcm4che.version,dcm4chee-storage.version | grep -v '^Downloaded' | grep -v '^Downloading' | grep -v '^Props'
+mvn -P ossrh-down versions:update-properties -DincludeProperties=jdbc-jboss-modules.version,jxpath-jboss-module.version,dcm4che.version,dcm4chee-storage.version
 mvn -P ossrh-down,db-all install -DskipTests=true
