480c2172b13d398114efc9ddbb501fd3d2f2c395
gunterze <gunterze@gmail.com>
2015-02-16 09:11:59 +0100

    refactor export script

--- dcm4chee-arc-conf/src/main/config/ldap/export.sh
+++ dcm4chee-arc-conf/src/main/config/ldap/export.sh
@@ -1,9 +1,19 @@
-# does not work...
-# . ../../../../../dcm4chee-arc-conf-test/src/test/filters/opendj.properties
+#!/bin/sh
 
-LDAP_BASE=/C/Tools/OpenDJ-2.5.0-Xpress1/bin
-$LDAP_BASE/ldapsearch -D "cn=Directory Manager" -w1 -b"cn=Devices,cn=DICOM Configuration,dc=example,dc=com"  "!(objectclass=dicomDevicesRoot)" > sample-config.ldif
+LDAP_SEARCH=${LDAP_SEARCH:-$HOME/opendj/bin/ldapsearch}
+SED=${SED:-sed}
+HOST=${HOST:-localhost}
+PORT=${PORT:-1389}
+ROOT_DN=${ROOT_DN:-"dc=example,dc=com"}
+BIND_DN=${BIND_DN:-"cn=Directory Manager"}
+BIND_PW=${BIND_PW:-secret}
 
-sed -nf unldif.sed sample-config.ldif > sample-config.ldif.processed
-rm sample-config.ldif
-mv sample-config.ldif.processed sample-config.ldif
+BASE_DN="cn=Devices,cn=DICOM Configuration,$ROOT_DN"
+FILTER='(!(objectclass=dicomDevicesRoot))'
+
+DIRNAME="`dirname "$0"`"
+UNLDIF_SED=$DIRNAME/unldif.sed
+SAMPLE_CONFIG_LDIF=$DIRNAME/sample-config.ldif
+
+$LDAP_SEARCH -h $HOST -p $PORT -D "$BIND_DN" -w $BIND_PW -b "$BASE_DN" "$FILTER" \
+| $SED -nf $UNLDIF_SED > $SAMPLE_CONFIG_LDIF
