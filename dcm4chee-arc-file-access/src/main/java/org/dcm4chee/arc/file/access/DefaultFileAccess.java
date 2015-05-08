6497f0e806c2477cb6a87325857fdb445e52cbcf
Umberto Cappellini <umberto.cappellini@agfa.com>
2015-05-08 09:19:06 +0200

    [AGCSP-16] Create an API for direct access to stored DICOM files

---
+++ dcm4chee-arc-file-access/src/main/java/org/dcm4chee/arc/file/access/DefaultFileAccess.java
@@ -0,0 +1,140 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.dcm4chee.arc.file.access;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.dcm4chee.archive.conf.QueryParam;
+import org.dcm4chee.archive.conf.QueryRetrieveView;
+import org.dcm4chee.archive.dto.ArchiveInstanceLocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Umberto Cappellini <umberto.cappellini@agfa.com>
+ *
+ */
+@ApplicationScoped
+public class DefaultFileAccess implements FileAccessService {
+
+    private static final Logger LOG = LoggerFactory
+            .getLogger(DefaultFileAccess.class);
+
+    @Inject
+    private  org.dcm4chee.archive.retrieve.RetrieveService archiveRetrieveService;
+    
+    @Inject
+    private org.dcm4chee.storage.service.RetrieveService storageRetrieveService;
+
+    public List<Path> getStudy (String uid) throws IOException {
+
+        List<ArchiveInstanceLocator> ails = 
+                archiveRetrieveService.calculateMatches(uid, null, null, param(), false);
+        
+        if (ails == null || ails.size() == 0)
+            return null;
+        
+        List<Path> res = new ArrayList<Path>();
+        
+        for (ArchiveInstanceLocator ail : ails)
+            res.add(getFile(ail));
+        
+        return res;
+    }
+
+    
+    public List<Path> getSeries (String uid) throws IOException {
+
+        List<ArchiveInstanceLocator> ails = 
+                archiveRetrieveService.calculateMatches(null, uid, null, param(), false);
+        
+        if (ails == null || ails.size() == 0)
+            return null;
+        
+        List<Path> res = new ArrayList<Path>();
+        
+        for (ArchiveInstanceLocator ail : ails)
+            res.add(getFile(ail));
+        
+        return res;
+    }
+
+    
+    public Path getInstance (String uid) throws IOException {
+
+        List<ArchiveInstanceLocator> ails = 
+                archiveRetrieveService.calculateMatches(null, null, uid, param(), false);
+        
+        if (ails == null || ails.size() == 0)
+            return null;
+        
+        return getFile(ails.get(0));
+    }
+    
+    private QueryParam param() {
+        QueryParam param = new QueryParam();
+        param.setMatchLinkedPatientIDs(false);
+        param.setQueryRetrieveView(new QueryRetrieveView());
+        return param;
+    }
+    
+    private Path getFile(ArchiveInstanceLocator inst) throws IOException {
+
+        ArchiveInstanceLocator archInst = (ArchiveInstanceLocator) inst;
+
+        org.dcm4chee.storage.RetrieveContext ctx = storageRetrieveService
+                .createRetrieveContext(archInst.getStorageSystem());
+        try {
+            return archInst.getEntryName() == null ? storageRetrieveService
+                    .getFile(ctx, archInst.getFilePath())
+                    : storageRetrieveService.getFile(ctx,
+                            archInst.getFilePath(), archInst.getEntryName());
+        } catch (InterruptedException e) {
+            throw new InterruptedIOException();
+        }
+    }
+}
---
+++ dcm4chee-arc-file-access/src/test/java/org/dcm4chee/arc/file/access/GetInstanceTestForIT.java
@@ -0,0 +1,102 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.dcm4chee.arc.file.access;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import javax.inject.Inject;
+
+import org.apache.log4j.Logger;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Umberto Cappellini <umberto.cappellini@agfa.com>
+ *
+ */
+@RunWith(Arquillian.class)
+public class GetInstanceTestForIT {
+
+    private static final Logger log = Logger.getLogger(GetInstanceTestForIT.class);
+
+    private static String sopUID = "1.2.840.113674.950809132347178.100";
+    private static Path dest =  Paths.get("/tmp/dicom.dcm");
+    
+    @Inject
+    private FileAccessService service;
+    
+    @Deployment @OverProtocol("Servlet 3.0")
+    public static WebArchive createDeployment() {
+        WebArchive war= ShrinkWrap.create(WebArchive.class, "test.war");
+        war.addClass(GetInstanceTestForIT.class);
+        JavaArchive[] archs =   Maven.resolver()
+                .loadPomFromFile("testpom.xml")
+                .importRuntimeAndTestDependencies()
+                .resolve().withoutTransitivity()
+                .as(JavaArchive.class);
+        for(JavaArchive a: archs) {
+            a.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+            war.addAsLibrary(a);
+        }
+        return war;
+    }
+    
+    @Test
+    public void testLoadInstance() throws Exception {
+        log.info("Started test:testLoadInstance");
+        Path path = service.getInstance(sopUID);
+        log.info("path:"+path);        
+        Files.copy(path, dest);
+        log.info("written file to " + dest);
+    }
+    
+
+}
