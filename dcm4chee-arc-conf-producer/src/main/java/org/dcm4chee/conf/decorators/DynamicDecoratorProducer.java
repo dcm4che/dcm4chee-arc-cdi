4e67cf317d601cf51f3f87121d8114d2022e9c56
Greg Knox <greg.knox@agfa.com>
2015-06-04 15:48:22 -0400

    [ARCH-282] Changing the QueryService, RetrieveService, and CStoreSCUService to use dynamic decorators

--- dcm4chee-arc-conf-producer/src/main/java/org/dcm4chee/conf/decorators/DynamicDecoratorProducer.java
+++ dcm4chee-arc-conf-producer/src/main/java/org/dcm4chee/conf/decorators/DynamicDecoratorProducer.java
@@ -1,7 +1,10 @@
 package org.dcm4chee.conf.decorators;
 
 import org.dcm4chee.archive.mpps.MPPSService;
+import org.dcm4chee.archive.query.QueryService;
+import org.dcm4chee.archive.retrieve.RetrieveService;
 import org.dcm4chee.archive.store.StoreService;
+import org.dcm4chee.archive.store.scu.CStoreSCUService;
 import org.dcm4chee.conf.decorators.ConfiguredDynamicDecorators;
 import org.dcm4chee.conf.decorators.DelegatingServiceImpl;
 import org.dcm4chee.conf.decorators.DynamicDecorator;
@@ -18,7 +21,6 @@ import java.util.Collection;
 
 @ApplicationScoped
 public class DynamicDecoratorProducer {
-	private static final Logger LOG = LoggerFactory.getLogger(DynamicDecoratorProducer.class);
 	
 	@Inject 
 	@DynamicDecorator
@@ -29,10 +31,31 @@ public class DynamicDecoratorProducer {
 	Instance<DelegatingServiceImpl<MPPSService>> dynamicMPPSDecorators;
 	
 	@Inject
+	@DynamicDecorator
+	Instance<DelegatingServiceImpl<QueryService>> dynamicQueryDecorators;
+	
+	@Inject
+	@DynamicDecorator
+	Instance<DelegatingServiceImpl<RetrieveService>> dynamicRetrieveDecorators;
+	
+	@Inject
+	@DynamicDecorator
+	Instance<DelegatingServiceImpl<CStoreSCUService>> dynamicCStoreSCUDecorators;
+	
+	@Inject
 	private ServiceDecorator<StoreService> storeDecorators;
 	
 	@Inject
 	private ServiceDecorator<MPPSService> mppsDecorators;
+	
+	@Inject
+	private ServiceDecorator<QueryService> queryDecorators;
+	
+	@Inject
+	private ServiceDecorator<RetrieveService> retrieveDecorators;
+	
+	@Inject
+	private ServiceDecorator<CStoreSCUService> cstoreSCUDecorators;
 	
 	@Produces
 	@ConfiguredDynamicDecorators
@@ -44,6 +67,24 @@ public class DynamicDecoratorProducer {
 	@ConfiguredDynamicDecorators
 	public Collection<DelegatingServiceImpl<MPPSService>> getConfiguredMPPSServiceDynamicDecorators() {
 		return mppsDecorators.getOrderedDecorators(dynamicMPPSDecorators, MPPSService.class.getName());
+	}
+	
+	@Produces
+	@ConfiguredDynamicDecorators
+	public Collection<DelegatingServiceImpl<QueryService>> getConfiguredQueryServiceDynamicDecorators() {
+		return queryDecorators.getOrderedDecorators(dynamicQueryDecorators, QueryService.class.getName());
+	}
+	
+	@Produces
+	@ConfiguredDynamicDecorators
+	public Collection<DelegatingServiceImpl<RetrieveService>> getConfiguredRetrieveServiceDynamicDecorators() {
+		return retrieveDecorators.getOrderedDecorators(dynamicRetrieveDecorators, RetrieveService.class.getName());
+	}
+	
+	@Produces
+	@ConfiguredDynamicDecorators
+	public Collection<DelegatingServiceImpl<CStoreSCUService>> getConfiguredCStoreSCUServiceDynamicDecorators() {
+		return cstoreSCUDecorators.getOrderedDecorators(dynamicCStoreSCUDecorators, CStoreSCUService.class.getName());
 	}
 	
 }
---
+++ dcm4chee-arc-store-scu/src/main/java/org/dcm4chee/archive/store/scu/decorators/DelegatingCStoreSCUService.java
@@ -0,0 +1,59 @@
+package org.dcm4chee.archive.store.scu.decorators;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.net.service.DicomServiceException;
+import org.dcm4chee.archive.dto.ArchiveInstanceLocator;
+import org.dcm4chee.archive.fetch.forward.FetchForwardService;
+import org.dcm4chee.archive.store.scu.CStoreSCUContext;
+import org.dcm4chee.archive.store.scu.CStoreSCUService;
+import org.dcm4chee.conf.decorators.DelegatingService;
+import org.dcm4chee.conf.decorators.DelegatingServiceImpl;
+
+@DelegatingService
+public class DelegatingCStoreSCUService extends DelegatingServiceImpl<CStoreSCUService> implements CStoreSCUService {
+
+	@Override
+	public void cstore(String messageID, CStoreSCUContext context, List<ArchiveInstanceLocator> insts, int priority) throws DicomServiceException {
+		getNextDecorator().cstore(messageID, context, insts, priority);
+	}
+
+	@Override
+	public void scheduleStoreSCU(String messageID, CStoreSCUContext context, List<ArchiveInstanceLocator> insts, int retries, int priority,	long delay) {
+		getNextDecorator().scheduleStoreSCU(messageID, context, insts, retries, priority, delay);
+	}
+
+	@Override
+	public void coerceAttributes(Attributes attrs, CStoreSCUContext context) throws DicomServiceException {
+		getNextDecorator().coerceAttributes(attrs, context);
+	}
+
+	@Override
+	public void coerceFileBeforeMerge(ArchiveInstanceLocator inst, Attributes attrs, CStoreSCUContext context) throws DicomServiceException {
+		getNextDecorator().coerceFileBeforeMerge(inst, attrs, context);
+	}
+
+	@Override
+	public ArchiveInstanceLocator applySuppressionCriteria(ArchiveInstanceLocator ref, Attributes attrs, String supressionCriteriaTemplateURI, CStoreSCUContext context) {
+		return getNextDecorator().applySuppressionCriteria(ref, attrs, supressionCriteriaTemplateURI, context);
+	}
+
+	@Override
+	public ArchiveInstanceLocator eliminateUnSupportedSOPClasses(ArchiveInstanceLocator ref, CStoreSCUContext context) {
+		return getNextDecorator().eliminateUnSupportedSOPClasses(ref, context);
+	}
+
+	@Override
+	public Path getFile(ArchiveInstanceLocator inst) throws IOException {
+		return getNextDecorator().getFile(inst);
+	}
+
+	@Override
+	public FetchForwardService getFetchForwardService() {
+		return getNextDecorator().getFetchForwardService();
+	}
+
+}
