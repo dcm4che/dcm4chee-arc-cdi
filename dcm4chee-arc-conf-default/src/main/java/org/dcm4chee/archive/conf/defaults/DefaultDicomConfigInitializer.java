0a437796198025204d9f66726e3f54cc4d54c43e
roman-k <roman.khazankin@gmail.com>
2015-07-02 16:43:48 +0200

    [logging] debug output on config init

--- dcm4chee-arc-conf-default/src/main/java/org/dcm4chee/archive/conf/defaults/DefaultDicomConfigInitializer.java
+++ dcm4chee-arc-conf-default/src/main/java/org/dcm4chee/archive/conf/defaults/DefaultDicomConfigInitializer.java
@@ -5,11 +5,16 @@ import org.dcm4che3.conf.api.TCConfiguration;
 import org.dcm4che3.conf.api.hl7.HL7Configuration;
 import org.dcm4che3.net.Connection;
 import org.dcm4che3.net.Device;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Roman K
  */
 public class DefaultDicomConfigInitializer {
+
+    private static Logger log = LoggerFactory.getLogger(DefaultDicomConfigInitializer.class);
+
     private Device arrDevice;
     private Device arc;
 
@@ -31,28 +36,41 @@ public class DefaultDicomConfigInitializer {
                                                               boolean isUseGroupBasedTCConfig) throws Exception {
 
 
+        log.debug("Creating external devices with AEs");
+
         for (int i = 0; i < DefaultDeviceFactory.OTHER_AES.length; i++) {
             String aet = DefaultDeviceFactory.OTHER_AES[i];
             config.persist(DefaultDeviceFactory.createDevice(DefaultDeviceFactory.OTHER_DEVICES[i], DefaultDeviceFactory.OTHER_ISSUER[i], DefaultDeviceFactory.OTHER_INST_CODES[i],
                     aet, "localhost", DefaultDeviceFactory.OTHER_PORTS[i << 1], DefaultDeviceFactory.OTHER_PORTS[(i << 1) + 1]));
         }
+
+        log.debug("Creating other external devices");
+
         hl7Config.registerHL7Application(DefaultDeviceFactory.PIX_MANAGER);
         for (int i = DefaultDeviceFactory.OTHER_AES.length; i < DefaultDeviceFactory.OTHER_DEVICES.length; i++)
             config.persist(DefaultDeviceFactory.createDevice(DefaultDeviceFactory.OTHER_DEVICES[i]));
+
         config.persist(DefaultDeviceFactory.createHL7Device("hl7rcv", DefaultDeviceFactory.SITE_A, DefaultDeviceFactory.INST_A, DefaultDeviceFactory.PIX_MANAGER,
                 "localhost", 2576, 12576));
+
+        log.debug("Creating arr device");
         arrDevice = DefaultDeviceFactory.createARRDevice("syslog", Connection.Protocol.SYSLOG_UDP, 514);
         config.persist(arrDevice);
 
+        log.debug("Creating archive device");
         DefaultDeviceFactory defaultDeviceFactory = new DefaultDeviceFactory();
         defaultDeviceFactory.setBaseStoragePath(baseStoragePath);
         defaultDeviceFactory.setUseGroupBasedTCConfig(isUseGroupBasedTCConfig);
         arc = defaultDeviceFactory.createArchiveDevice("dcm4chee-arc", arrDevice);
         config.persist(arc);
 
+
+
         // create TC Group config extension
-        if (isUseGroupBasedTCConfig)
+        if (isUseGroupBasedTCConfig) {
+            log.debug("Creating transfer capability groups");
             TCConfiguration.persistDefaultTCGroups(config);
+        }
 
         return this;
     }
