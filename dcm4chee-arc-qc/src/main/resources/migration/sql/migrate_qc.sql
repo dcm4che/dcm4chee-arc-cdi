4292dc0d2f486d21670273cbc318b8328ba367f2
Umberto Cappellini <umberto.cappellini@agfa.com>
2015-12-21 14:44:16 +0100

    refactor: [DCMEEREQ-1186] Create an EJB for manipulating history tables

--- dcm4chee-arc-qc/src/main/resources/migration/sql/migrate_qc.sql
+++ dcm4chee-arc-qc/src/main/resources/migration/sql/migrate_qc.sql
@@ -1,80 +1,80 @@
 /* start QC ACTION History */
-ALTER TABLE qc_action RENAME qc_action_history;
+ALTER TABLE action RENAME action_history;
 
-ALTER TABLE qc_action_history
+ALTER TABLE action_history
 DROP TRANSACTION_ID;
 
-ALTER TABLE qc_action_history CHANGE ACTION action VARCHAR(255) NOT NULL;
+ALTER TABLE action_history CHANGE ACTION action VARCHAR(255) NOT NULL;
 
-ALTER TABLE qc_action_history CHANGE DB_CREATED_TIME created_time datetime NOT NULL;
+ALTER TABLE action_history CHANGE DB_CREATED_TIME created_time datetime NOT NULL;
 
- LOCK TABLES qc_study_history WRITE,
- qc_action_history WRITE;
+ LOCK TABLES study_history WRITE,
+ action_history WRITE;
 
-ALTER TABLE qc_study_history
+ALTER TABLE study_history
 DROP
-FOREIGN KEY QC_STUDY_HIST_ACTION_FK;
+FOREIGN KEY STUDY_HIST_ACTION_FK;
 
-ALTER TABLE qc_study_history
-DROP INDEX QC_STUDY_HIST_ACTION_FK;
+ALTER TABLE study_history
+DROP INDEX STUDY_HIST_ACTION_FK;
 
-ALTER TABLE qc_action_history CHANGE PK pk bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE action_history CHANGE PK pk bigint(20) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE qc_study_history CHANGE QC_ACTION_FK qc_action_history_fk bigint(20);
+ALTER TABLE study_history CHANGE ACTION_FK action_history_fk bigint(20);
 
-ALTER TABLE qc_study_history ADD CONSTRAINT FK154398B16C9B9292
-FOREIGN KEY (qc_action_history_fk) REFERENCES qc_action_history (pk);
+ALTER TABLE study_history ADD CONSTRAINT FK154398B16C9B9292
+FOREIGN KEY (action_history_fk) REFERENCES action_history (pk);
 
  UNLOCK TABLES;
  
 /* end QC ACTION History */
  
  /* start QC STUDY History */
-ALTER TABLE qc_study_history CHANGE OLD_STUDY_IUID old_study_uid varchar(255) NOT NULL;
+ALTER TABLE study_history CHANGE OLD_STUDY_IUID old_study_uid varchar(255) NOT NULL;
 
-ALTER TABLE qc_study_history CHANGE NEW_STUDY_IUID next_study_uid varchar(255) NULL DEFAULT NULL;
+ALTER TABLE study_history CHANGE NEW_STUDY_IUID next_study_uid varchar(255) NULL DEFAULT NULL;
 
- LOCK TABLES qc_series_history WRITE,
- qc_study_history WRITE;
+ LOCK TABLES series_history WRITE,
+ study_history WRITE;
 
-ALTER TABLE qc_series_history
+ALTER TABLE series_history
 DROP
-FOREIGN KEY QC_SERIES_HIST_STUDY_HIST_FK;
+FOREIGN KEY SERIES_HIST_STUDY_HIST_FK;
 
-ALTER TABLE qc_series_history
-DROP INDEX QC_SERIES_HIST_STUDY_HIST_FK;
+ALTER TABLE series_history
+DROP INDEX SERIES_HIST_STUDY_HIST_FK;
 
-ALTER TABLE qc_study_history CHANGE PK pk bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE study_history CHANGE PK pk bigint(20) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE qc_series_history CHANGE OLD_STUDY_HIST_FK qc_study_history_fk bigint(20);
+ALTER TABLE series_history CHANGE OLD_STUDY_HIST_FK study_history_fk bigint(20);
 
-ALTER TABLE qc_series_history ADD CONSTRAINT FKFD935C99C2EE0096
-FOREIGN KEY (qc_study_history_fk) REFERENCES qc_study_history (pk);
+ALTER TABLE series_history ADD CONSTRAINT FKFD935C99C2EE0096
+FOREIGN KEY (study_history_fk) REFERENCES study_history (pk);
 
 UNLOCK TABLES;
 
  /* start attributes  procedure */
-ALTER TABLE dicomattrs ADD qc_study_history_fk bigint(20);
+ALTER TABLE dicomattrs ADD study_history_fk bigint(20);
 
-ALTER TABLE qc_study_history ADD dicomattrs_fk bigint(20);
+ALTER TABLE study_history ADD dicomattrs_fk bigint(20);
 
-ALTER TABLE qc_study_history ADD CONSTRAINT FK154398B185AF69D8
+ALTER TABLE study_history ADD CONSTRAINT FK154398B185AF69D8
 FOREIGN KEY (dicomattrs_fk) REFERENCES dicomattrs (pk);
 
-INSERT INTO dicomattrs (attrs, qc_study_history_fk)
+INSERT INTO dicomattrs (attrs, study_history_fk)
 SELECT STUDY_ATTRS,
        pk
-FROM qc_study_history;
+FROM study_history;
 
-UPDATE qc_study_history,
+UPDATE study_history,
        dicomattrs
 SET dicomattrs_fk = dicomattrs.pk
-WHERE qc_study_history.pk = dicomattrs.qc_study_history_fk;
+WHERE study_history.pk = dicomattrs.study_history_fk;
 
 ALTER TABLE dicomattrs
-DROP qc_study_history_fk;
+DROP study_history_fk;
 
-ALTER TABLE qc_study_history
+ALTER TABLE study_history
 DROP STUDY_ATTRS;
 
  /* end attributes  procedure */ 
@@ -82,100 +82,100 @@ DROP STUDY_ATTRS;
  /* end QC STUDY History */
  
  /* start QC SERIES History */
-ALTER TABLE qc_series_history CHANGE OLD_SERIES_IUID old_series_iuid varchar(255);
+ALTER TABLE series_history CHANGE OLD_SERIES_IUID old_series_iuid varchar(255);
 
-ALTER TABLE qc_series_history
+ALTER TABLE series_history
 DROP NEW_SERIES_IUID;
 
- LOCK TABLES qc_instance_history WRITE,
- qc_series_history WRITE;
+ LOCK TABLES instance_history WRITE,
+ series_history WRITE;
 
-ALTER TABLE qc_instance_history
+ALTER TABLE instance_history
 DROP
-FOREIGN KEY QC_INST_HIST_SERIES_HIST_FK;
+FOREIGN KEY INST_HIST_SERIES_HIST_FK;
 
-ALTER TABLE qc_instance_history
-DROP INDEX QC_INST_HIST_SERIES_HIST_FK;
+ALTER TABLE instance_history
+DROP INDEX INST_HIST_SERIES_HIST_FK;
 
-ALTER TABLE qc_series_history CHANGE PK pk bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE series_history CHANGE PK pk bigint(20) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE qc_instance_history CHANGE OLD_SERIES_HIST_FK qc_series_history_fk bigint(20);
+ALTER TABLE instance_history CHANGE OLD_SERIES_HIST_FK series_history_fk bigint(20);
 
-ALTER TABLE qc_instance_history ADD CONSTRAINT FK68FB577E4DDF450
-FOREIGN KEY (qc_series_history_fk) REFERENCES qc_series_history (pk);
+ALTER TABLE instance_history ADD CONSTRAINT FK68FB577E4DDF450
+FOREIGN KEY (series_history_fk) REFERENCES series_history (pk);
 
  UNLOCK TABLES;
 
  /* start attributes  procedure */
-ALTER TABLE dicomattrs ADD qc_series_history_fk bigint(20);
+ALTER TABLE dicomattrs ADD series_history_fk bigint(20);
 
-ALTER TABLE qc_series_history ADD dicomattrs_fk bigint(20);
+ALTER TABLE series_history ADD dicomattrs_fk bigint(20);
 
-ALTER TABLE qc_series_history ADD CONSTRAINT FKFD935C9985AF69D8
+ALTER TABLE series_history ADD CONSTRAINT FKFD935C9985AF69D8
 FOREIGN KEY (dicomattrs_fk) REFERENCES dicomattrs (pk);
 
-INSERT INTO dicomattrs (attrs, qc_series_history_fk)
+INSERT INTO dicomattrs (attrs, series_history_fk)
 SELECT BIN(pk),
        pk
-FROM qc_series_history;
+FROM series_history;
 
-UPDATE qc_series_history,
+UPDATE series_history,
        dicomattrs
 SET dicomattrs_fk = dicomattrs.pk
-WHERE qc_series_history.pk = dicomattrs.qc_series_history_fk;
+WHERE series_history.pk = dicomattrs.series_history_fk;
 
 ALTER TABLE dicomattrs
-DROP qc_series_history_fk;
+DROP series_history_fk;
 
  /* end attributes  procedure */ 
  
  /* end QC SERIES History */
   
  /* start QC Instance History */
-ALTER TABLE qc_instance_history CHANGE CREATED_FOR_CLONE cloned bit(1) NOT NULL DEFAULT 0;
+ALTER TABLE instance_history CHANGE CREATED_FOR_CLONE cloned bit(1) NOT NULL DEFAULT 0;
 
-ALTER TABLE qc_instance_history CHANGE CURRENT_IUID current_uid varchar(255) NOT NULL;
+ALTER TABLE instance_history CHANGE CURRENT_IUID current_uid varchar(255) NOT NULL;
 
-ALTER TABLE qc_instance_history CHANGE NEXT_IUID next_uid varchar(255) NOT NULL;
+ALTER TABLE instance_history CHANGE NEXT_IUID next_uid varchar(255) NOT NULL;
 
-ALTER TABLE qc_instance_history CHANGE OBSOLETE_IUID old_uid varchar(255) NOT NULL;
+ALTER TABLE instance_history CHANGE OBSOLETE_IUID old_uid varchar(255) NOT NULL;
 
-ALTER TABLE qc_instance_history CHANGE PK pk bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE instance_history CHANGE PK pk bigint(20) NOT NULL AUTO_INCREMENT;
 
  /* start attributes  procedure */
-ALTER TABLE dicomattrs ADD qc_instance_history_fk bigint(20);
+ALTER TABLE dicomattrs ADD instance_history_fk bigint(20);
 
-ALTER TABLE qc_instance_history ADD dicomattrs_fk bigint(20);
+ALTER TABLE instance_history ADD dicomattrs_fk bigint(20);
 
-ALTER TABLE qc_instance_history ADD CONSTRAINT FK68FB57785AF69D8
+ALTER TABLE instance_history ADD CONSTRAINT FK68FB57785AF69D8
 FOREIGN KEY (dicomattrs_fk) REFERENCES dicomattrs (pk);
 
-INSERT INTO dicomattrs (attrs, qc_instance_history_fk)
+INSERT INTO dicomattrs (attrs, instance_history_fk)
 SELECT BIN(pk),
        pk
-FROM qc_instance_history;
+FROM instance_history;
 
-UPDATE qc_instance_history,
+UPDATE instance_history,
        dicomattrs
 SET dicomattrs_fk = dicomattrs.pk
-WHERE qc_instance_history.pk = dicomattrs.qc_instance_history_fk;
+WHERE instance_history.pk = dicomattrs.instance_history_fk;
 
 ALTER TABLE dicomattrs
-DROP qc_instance_history_fk;
+DROP instance_history_fk;
 
  /* end attributes  procedure */ 
  /* end QC Instance History */
  
  /* Add current_study_uid and current_series_uid*/
-ALTER TABLE qc_instance_history ADD current_study_uid varchar(255) NOT NULL;
+ALTER TABLE instance_history ADD current_study_uid varchar(255) NOT NULL;
 
-ALTER TABLE qc_instance_history ADD current_series_uid varchar(255) NOT NULL;
+ALTER TABLE instance_history ADD current_series_uid varchar(255) NOT NULL;
 
-/*INSERT INTO qc_instance_history (current_study_uid, current_series_uid)
+/*INSERT INTO instance_history (current_study_uid, current_series_uid)
 SELECT st.study_iuid,sr.series_iuid FROM instance inst
 join series sr join study st
-left outer join qc_instance_history qci on qci.old_uid=inst.sop_iuid;*/
-CREATE INDEX qc_instance_history_old_uid_idx ON qc_instance_history (old_uid);
-CREATE INDEX qc_instance_history_next_uid_idx ON qc_instance_history (next_uid);
-CREATE INDEX qc_instance_history_current_uid_idx ON qc_instance_history (current_uid);
-CREATE INDEX qc_action_history_created_time_idx ON qc_action_history (created_time);
+left outer join instance_history qci on qci.old_uid=inst.sop_iuid;*/
+CREATE INDEX instance_history_old_uid_idx ON instance_history (old_uid);
+CREATE INDEX instance_history_next_uid_idx ON instance_history (next_uid);
+CREATE INDEX instance_history_current_uid_idx ON instance_history (current_uid);
+CREATE INDEX action_history_created_time_idx ON action_history (created_time);
